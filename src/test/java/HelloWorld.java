5b36dc548e559cc7858a834002fa883ceb0b7502
hP <hP@test.com>
2022-07-02 19:53:03 +0530

    1st

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+
+    static void printHello(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[]args){
+        printHello();
+    }
+}
