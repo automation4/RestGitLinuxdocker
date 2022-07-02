2b00345b0e49d023a55a1ba8f14c77c6df2ea388
hP <hP@test.com>
2022-07-02 20:16:45 +0530

    3rd

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     static void printHello(){
         System.out.println("Hello world");
-        System.out.println("Rests");
+        System.out.println("Rests is bests");
     }
 
     public static void main(String[]args){
